d96dfe7946545a826455ecb829abe131c96e87b2
ashwanikr7499 <34263444+ashwanikr7499@users.noreply.github.com>
2022-10-25 23:01:26 +0530

    init

---
+++ src/factoryDesignPattern/MainClass.java
@@ -0,0 +1,13 @@
+package factoryDesignPattern;
+
+import java.util.Scanner;
+
+public class MainClass {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter type of notification");
+        String type=sc.nextLine();
+        NotificationService notificationService=NotificationFactory.getNotificationService(type);
+        notificationService.notifyUser();
+    }
+}
